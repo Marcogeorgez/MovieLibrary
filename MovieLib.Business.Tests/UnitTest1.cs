21fe2fa1be591a7ab1dafd528cadf2a32ac2f214
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-08-27 12:00:46 +0300

    Add project files.

---
+++ MovieLib.Business.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MovieLib.Business.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
---
+++ MovieLib.Business/Migrations/20240804175101_renamingTables.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieLib.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamingTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MoviesEF",
+                table: "MoviesEF");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GenresEF",
+                table: "GenresEF");
+
+            migrationBuilder.RenameTable(
+                name: "MoviesEF",
+                newName: "Movies");
+
+            migrationBuilder.RenameTable(
+                name: "GenresEF",
+                newName: "Genre");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Movies",
+                newName: "MoviesEF");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "GenresEF");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MoviesEF",
+                table: "MoviesEF",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GenresEF",
+                table: "GenresEF",
+                column: "Id");
+        }
+    }
+}
