363933457a2a4a702bc90f26a429f06f35e7273b
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-09-08 01:41:21 +0300

    feat : add RequestTimingMiddleware for measuring request duration
    -Implemented RequestTimingMiddleware to measure the duration of HTTP requests using Stopwatch.
    -Middleware logs the elapsed time in milliseconds to the console after each request.
    -Added an extension method UseRequestTiming to simplify middleware registration.
    -Integrated middleware into the development pipeline with app.UseRequestTiming().

---
+++ MovieLib.WebApplication/Middleware/RequestTimingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace MovieLib.WebApplication.Middleware;
+
+public class RequestTimingMiddleware
+{
+	private readonly RequestDelegate _next;
+	public RequestTimingMiddleware(RequestDelegate next)
+	{
+		_next = next;
+	}
+	public async Task InvokeAsync(HttpContext ctx)
+	{
+		var stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		await _next(ctx);
+
+		stopwatch.Stop();
+
+		Console.WriteLine($"Request took:{stopwatch.ElapsedMilliseconds} ms");
+
+	}
+}
+public static class RequestTimingMiddlewareExtensions
+{
+	public static IApplicationBuilder UseRequestTiming(this IApplicationBuilder builder)
+	{
+		return builder.UseMiddleware<RequestTimingMiddleware>();
+	}
+}
+
