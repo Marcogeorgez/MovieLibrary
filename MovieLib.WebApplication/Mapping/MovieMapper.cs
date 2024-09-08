db07dfe31dbc4086d6bd694d678cb4cee8c9fb79
Marco George <58277280+Marcogeorgez@users.noreply.github.com>
2024-09-08 04:59:16 +0300

    - Refactor: Moved MovieMapper from Business Layer to PresentationLayer

---
+++ MovieLib.WebApplication/Mapping/MovieMapper.cs
@@ -0,0 +1,33 @@
+﻿using DTOs;
+using MovieLib.Domain;
+
+namespace MovieLib.WebApplication.Mapping;
+
+public static class MovieMapper
+{
+	public static MovieGetDTO ToMovieGetDTO(Movie movie)
+	{
+		return new MovieGetDTO
+		{
+			Title = movie!.Title,
+			Plot = movie.Plot,
+			WatchedDate = movie.WatchedDate,
+			Seen = movie.Seen,
+			Rating = movie.Rating,
+			GenreNames = movie.GenreNames
+		};
+	}
+	public static Movie ToMovieFromMovieCreateDTO(MovieCreateDTO movie)
+	{
+		return new Movie()
+		{
+			Title = movie.Title,
+			Plot = movie.Plot,
+			WatchedDate = movie.WatchedDate,
+			Seen = movie.Seen,
+			Rating = movie.Rating,
+			GenreIds = movie.GenreIds
+		};
+	}
+
+}
